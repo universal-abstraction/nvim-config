987ea8d061eb59f40c6ccc1ed27269c663b07bc6
Mark <absolute.abstraction.general@gmail.com>
2026-02-25 16:03:04 +0100

    fix: tab works for autocomplete selection.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,50 +4,50 @@
 -- Replaces nvim-cmp with 0.5-4ms overhead vs nvim-cmp's 60ms debounce
 
 return {
-  "saghen/blink.cmp",
-  version = "1.*",
-  event = "InsertEnter",
-  dependencies = {
-    { "L3MON4D3/LuaSnip", version = "v2.*" },
-    "rafamadriz/friendly-snippets",
-  },
+	"saghen/blink.cmp",
+	version = "1.*",
+	event = "InsertEnter",
+	dependencies = {
+		{ "L3MON4D3/LuaSnip", version = "v2.*" },
+		"rafamadriz/friendly-snippets",
+	},
 
-  opts = {
-    keymap = {
-      preset = "default",
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-e>"] = { "hide", "fallback" },
-      ["<CR>"] = { "accept", "fallback" },
-      ["<Tab>"] = { "snippet_forward", "fallback" },
-      ["<S-Tab>"] = { "snippet_backward", "fallback" },
-      ["<C-p>"] = { "select_prev", "fallback" },
-      ["<C-n>"] = { "select_next", "fallback" },
-      ["<Up>"] = { "select_prev", "fallback" },
-      ["<Down>"] = { "select_next", "fallback" },
-      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-    },
+	opts = {
+		keymap = {
+			preset = "default",
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
+			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback" },
+			["<C-n>"] = { "select_next", "fallback" },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+		},
 
-    appearance = {
-      nerd_font_variant = "mono",
-    },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
 
-    completion = {
-      accept = { auto_brackets = { enabled = true } },
-      documentation = { auto_show = true, auto_show_delay_ms = 200 },
-      list = { selection = { preselect = false, auto_insert = false } },
-    },
+		completion = {
+			accept = { auto_brackets = { enabled = true } },
+			documentation = { auto_show = true, auto_show_delay_ms = 200 },
+			list = { selection = { preselect = false, auto_insert = false } },
+		},
 
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
 
-    snippets = { preset = "luasnip" },
-  },
+		snippets = { preset = "luasnip" },
+	},
 
-  config = function(_, opts)
-    -- Load friendly-snippets into LuaSnip
-    require("luasnip.loaders.from_vscode").lazy_load()
-    require("blink.cmp").setup(opts)
-  end,
+	config = function(_, opts)
+		-- Load friendly-snippets into LuaSnip
+		require("luasnip.loaders.from_vscode").lazy_load()
+		require("blink.cmp").setup(opts)
+	end,
 }
--- lua/plugins/lang-autoswitch.lua
+++ lua/plugins/lang-autoswitch.lua
@@ -3,16 +3,18 @@
 -- Input layout autoswitch (Hyprland backend)
 
 return {
-  "universal-abstraction/lang-autoswitch.nvim",
-  event = { "VimEnter", "InsertEnter", "InsertLeave", "FocusGained", "FocusLost" },
-  config = function()
-    require("lang_autoswitch").setup({
-      default_layout = "us",
-      restore_on_insert = true,
-      set_on_focus_gained = true,
-      restore_on_focus_lost = true,
-      device = "at-translated-set-2-keyboard",
-      layouts = { "us", "ru" },
-    })
-  end,
+	"universal-abstraction/lang-autoswitch.nvim",
+	event = { "VimEnter", "InsertEnter", "InsertLeave", "FocusGained", "FocusLost" },
+	config = function()
+		require("lang_autoswitch").setup({
+			default_layout = "us",
+			restore_on_insert = true,
+			set_on_focus_gained = true,
+			restore_on_focus_lost = true,
+			device = "at-translated-set-2-keyboard",
+			layouts = { "us", "ru" },
+			--debug = true,
+			--debug_path = "/tmp/lang-autoswitch.log",
+		})
+	end,
 }
